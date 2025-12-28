595517f437a372ec8566b3e7394a1865a0a7ea20
VilinhaCobble <johnathanlima90@gmail.com>
2025-12-27 23:37:56 -0300

    v0.0.13
    Mudança títulos

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -7,7 +7,7 @@ const logger = LoggerUtil.getLogger('ConfigManager')
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-const dataPath = path.join(sysRoot, 'Cobblemon Vilinha2')
+const dataPath = path.join(sysRoot, 'Cobblemon Vilinha')
 
 const launcherDir = require('@electron/remote').app.getPath('userData')
 
